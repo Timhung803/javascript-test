f9839635a713a2b4489d012471c4acf25188f5df
Timhung803 <timhkt@gmail.com>
2022-03-13 02:47:08 +1300

    try to run js with node and learning variable

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-        // This is my first JavaScript code!
-        console.log('first code');
+// This is my first JavaScript code!
+console.log('first code');
+
+// learning variables
+let name = 'tim';
+console.log(name);
+
+let firstName = 'tim';
+console.log(firstName)
